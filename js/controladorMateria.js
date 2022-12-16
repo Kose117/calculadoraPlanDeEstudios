95452ee68cbbb0879e2025382840663e482eda22
Santicm23 <santicastrom@outlook.com>
2022-12-16 14:19:51 -0500

    añadiendo botones y ajustando la pantalla

--- js/controladorMateria.js
+++ js/controladorMateria.js
@@ -2,14 +2,16 @@
 
 window.addEventListener("load", function(){
 
-    var overlay = this.document.querySelector(".overlay"),
-        btnAbrir = this.document.querySelector("#abrir");
-        btnCerrar = this.document.querySelector("#cerrar")
-
-    btnAbrir.addEventListener("click", function(){
-        btnCerrar.classList.add("active");
-        overlay.classList.add("active");
-    });
+    var btnsMaterias = this.document.getElementsByClassName("btn-materias")
+        overlay = this.document.querySelector(".overlay"),
+        btnCerrar = this.document.querySelector("#cerrar");
+        for(var i = 0; i < btnsMaterias.length; i++){
+            btnsMaterias.item(i).addEventListener("click", function(){
+                btnCerrar.classList.add("active");
+                overlay.classList.add("active");
+            });
+        }
+        
 
     btnCerrar.addEventListener("click", function(){
         overlay.classList.remove("active");
