11eae07dd7e5d6912353d5466b91302f0751086e
Santicm23 <santicastrom@outlook.com>
2022-12-18 23:28:05 -0500

    leyendo archivo con servidor local

--- js/controladorMateria.js
+++ js/controladorMateria.js
@@ -1,16 +1,28 @@
 "using strict"
 
+var clases
+fetch('../archivos/clases.json')
+    .then(response => response.json())
+    .then(data =>{
+        clases = data;
+    });
+
 window.addEventListener("load", function(){
 
-    var btnsMaterias = this.document.getElementsByClassName("btn-materias")
-        overlay = this.document.querySelector(".overlay"),
-        btnCerrar = this.document.querySelector("#cerrar");
-        for(var i = 0; i < btnsMaterias.length; i++){
-            btnsMaterias.item(i).addEventListener("click", function(){
-                btnCerrar.classList.add("active");
-                overlay.classList.add("active");
-            });
-        }
+    var btnsMaterias = this.document.getElementsByClassName("btn-materias");
+    var overlay = this.document.querySelector(".overlay");
+    var btnCerrar = this.document.querySelector("#cerrar");
+    var materia = this.document.querySelector("#nombre-materia");
+    var codigo = this.document.querySelector("#codigo");
+
+    for (var i = 0; i < btnsMaterias.length; i++){
+        btnsMaterias.item(i).addEventListener("click", function(){
+            btnCerrar.classList.add("active");
+            overlay.classList.add("active");
+            materia.value = clases[this.id].nombre;
+            codigo.value = this.id;
+        });
+    }
     
     btnCerrar.addEventListener("click", function(){
         overlay.classList.remove("active");
