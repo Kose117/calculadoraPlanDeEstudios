0da6d4138ecd1d790a7267bf780a35981b8bdeb8
Santicm23 <santicastrom@outlook.com>
2023-01-03 15:35:43 -0500

    arreglando opacidad del popup

--- js/controladorTarjetas.js
+++ js/controladorTarjetas.js
@@ -1,3 +1,8 @@
+
+import './controladorMateria';
+
+// console.log({materias});
+
 /*  
 
 var containerCard=document.querySelector(".container-card");
@@ -16,90 +21,95 @@ var btn=document.querySelector(".btn");
 
 window.addEventListener("load", function(){
 
-var btnAnadir = this.document.querySelector("#boton");
-btnAnadir.addEventListener("click",crearTarjetas)
-{       
-}
-function crearTarjetas()
-{
-    var containerCard=document.querySelector(".container-card");
-                    
-    var cardFather=document.createElement("div");
-    cardFather.classList.add("card-father");
-    containerCard.appendChild(cardFather);
+    var btnAnadir = document.querySelector("#boton");
+    btnAnadir.addEventListener("click", crearTarjetas);
+
+    function crearTarjetas()
+    {
+        var containerCard=document.querySelector(".container-card");
+                        
+        var cardFather=document.createElement("div");
+        cardFather.classList.add("card-father");
+        containerCard.appendChild(cardFather);
+        
+        var card=document.createElement("div");
+        card.classList.add("card");
+        cardFather.appendChild(card);
+        
+        var cardFront=document.createElement("div");
+        cardFront.classList.add("card-front");
+        cardFront.style.backgroundImage= "url('../imagenes/mapu.jpg')";;
+        card.appendChild(cardFront);
+
+        var bg=document.createElement("div");
+        bg.classList.add("bg");
+        cardFront.append(bg);
+
+        var bodyCardFront=document.createElement("div");
+        bodyCardFront.classList.add("body-card-front");
+        cardFront.append(bodyCardFront);
+
+        var nombreClase=document.createElement("h1");
+        nombreClase.setAttribute('id',"Nombreclase");
+        nombreClase.innerText="Calculo integral";
+        bodyCardFront.appendChild(nombreClase);
+
+        var cardBack=document.createElement("div");
+        cardBack.classList.add("card-back");
+        cardBack.style.backgroundImage= "url('../imagenes/sorpresa.jpg')";;
+        card.appendChild(cardBack);
+
+        var bodyCardBack=document.createElement("div");
+        bodyCardBack.classList.add("body-card-back");
+        cardBack.append(bodyCardBack);
+
+        var clase=document.createElement("h1");
+        clase.classList.add("alejate");
+        clase.setAttribute('id',"clase");
+        clase.innerText="Clase";
+        bodyCardBack.appendChild(clase);
+
+        var nota=document.createElement("h2");
+        nota.classList.add("alejate");
+        nota.setAttribute('id',"nota");
+        nota.innerText="Nota:";
+        bodyCardBack.appendChild(nota);
+
+        var profesor=document.createElement("h2");
+        profesor.classList.add("alejate");
+        profesor.setAttribute('id',"profesor");
+        profesor.innerText="Profesor:";
+        bodyCardBack.appendChild(profesor);
+
+        var btn=document.createElement("button");
+        btn.classList.add("btn",'btn-abrir-popup');
+        // btn.setAttribute('id','btn-abrir-popup');
+        bodyCardBack.appendChild(btn);
+
+        var span=document.createElement("span");
+        span.innerText="Cálculo Diferencial";
+        btn.appendChild(span);
+        
+        // console.log(containerCard);    
+        var btnAbrirPopup = document.getElementsByClassName('btn-abrir-popup'),
+            overlay = document.querySelector(".overlay"),
+            popup = document.querySelector(".popup"),
+            btnCerrarPopup = document.getElementsByClassName('btn-cerrar-popup');
     
-    var card=document.createElement("div");
-    card.classList.add("card");
-    cardFather.appendChild(card);
+        // btnAbrirPopup?.addEventListener('click', function(){
+        //     overlay.classList.add('active');
+        //     popup.classList.add('active');
+        //     console.log(overlay);
+        // });
     
-    var cardFront=document.createElement("div");
-    cardFront.classList.add("card-front");
-    cardFront.style.backgroundImage= "url('../imagenes/mapu.jpg')";;
-    card.appendChild(cardFront);
+        for (let index = 0; index < btnAbrirPopup.length; index++) {
+            btnAbrirPopup[index].addEventListener('click', function(){
+                overlay.classList.add('active');
+                console.log(popup);
+            });
+        }
+    }
 
-    var bg=document.createElement("div");
-    bg.classList.add("bg");
-    cardFront.append(bg);
-
-    var bodyCardFront=document.createElement("div");
-    bodyCardFront.classList.add("body-card-front");
-    cardFront.append(bodyCardFront);
-
-    var nombreClase=document.createElement("h1");
-    nombreClase.setAttribute('id',"Nombreclase");
-    nombreClase.innerText="Calculo integral";
-    bodyCardFront.appendChild(nombreClase);
-
-    var cardBack=document.createElement("div");
-    cardBack.classList.add("card-back");
-    cardBack.style.backgroundImage= "url('../imagenes/sorpresa.jpg')";;
-    card.appendChild(cardBack);
-
-    var bodyCardBack=document.createElement("div");
-    bodyCardBack.classList.add("body-card-back");
-    cardBack.append(bodyCardBack);
-
-    var clase=document.createElement("h1");
-    clase.classList.add("alejate");
-    clase.setAttribute('id',"clase");
-    clase.innerText="Clase";
-    bodyCardBack.appendChild(clase);
-
-    var nota=document.createElement("h2");
-    nota.classList.add("alejate");
-    nota.setAttribute('id',"nota");
-    nota.innerText="Nota:";
-    bodyCardBack.appendChild(nota);
-
-    var profesor=document.createElement("h2");
-    profesor.classList.add("alejate");
-    profesor.setAttribute('id',"profesor");
-    profesor.innerText="Profesor:";
-    bodyCardBack.appendChild(profesor);
-
-    var btn=document.createElement("button");
-    btn.classList.add("btn");
-    btn.setAttribute('id','btn-abrir-popup');
-    bodyCardBack.appendChild(btn);
-
-    var span=document.createElement("span");
-    span.innerText="Cálculo Diferencial";
-    btn.appendChild(span);
-    
-    console.log(containerCard);
-    
-}
-/*SANTI MIERDAAAA NO ENTIENDO NADAAAA */
-    var btnAbrirPopup = this.document.getElementById('btn-abrir-popup'),
-	overlay = this.document.querySelector(".overlay"),
-	popup = this.document.querySelector(".popup"),
-	btnCerrarPopup = this.document.getElementById('btn-cerrar-popup');
-
-    btnAbrirPopup?.addEventListener('click', function(){
-	overlay.classList.add('active');
-	popup.classList.add('active');
-    console.log(overlay);
-});
-
+    /* TODO: SANTI MIERDAAAA NO ENTIENDO NADAAAA */
 });
  
