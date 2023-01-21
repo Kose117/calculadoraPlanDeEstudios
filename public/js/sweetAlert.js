c8da8515dd1be4341f57626e60a50174bf10b2ee
joseManuel1234 <jmanuelrardilla@gmail.com>
2023-01-21 15:33:47 -0500

    alertas

---
+++ public/js/sweetAlert.js
@@ -0,0 +1,28 @@
+// const correctamente = Swal.mixin({
+//     toast: true,
+//     position: 'bottom',
+//     showConfirmButton: false,
+//     timer: 3000,
+//     timerProgressBar: true,
+//     didOpen: (toast) => {
+//       toast.addEventListener('mouseenter', Swal.stopTimer)
+//       toast.addEventListener('mouseleave', Swal.resumeTimer)
+//     }
+//   })
+  
+//   correctamente.fire({
+//     icon: 'success',
+//     title: 'Se agregó correctamente'
+//   })
+
+// const error=Swal.fire({
+//     icon: 'warning',
+//     title: '¡Agregue un semestre valido!',
+//     confirmButtonText: 'Solo numeros :)',
+//     buttonsStyling:false,
+//     customClass: {
+//         popup: 'error',
+//         confirmButton: 'confirm-error',
+//         icon: 'icon-error',
+//         title:'title-error'
+// }})
